773fcaf0a2eb81860ca141feb7f7dcbcfe7550bc
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-18 16:37:58 +0530

    4_Arithematic_Operations updated

--- 4_Arithematic_Operations.c
+++ 4_Arithematic_Operations.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-int main() {
-    char operator;
-    double n1, n2;
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&n1, &n2);
+//Author AryanSharma 133
+#include <stdio.h> //stdio.h is the header file for standard input and output.
+int main(){        //It means that our will return some integer value at the end.
+    char operator; //Declaring operator variable
+    double n1, n2; //Declaring operator variable
+    printf("Enter an operator (+, -, *, /): ");	 //printf function calling
+    scanf("%c", &operator);	 //scanf function calling
+    printf("Enter two operands: ");	//printf function calling
+    scanf("%lf %lf",&n1, &n2);	//scanf function calling
     switch(operator){
         case '+':
-            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
+            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);//to be executed if operator = +;
             break; 
         case '-':
-            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
+            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);//to be executed if operator = -;
             break; 
         case '*':
-            printf("%.1lf * %.3lf = %.2lf",n1, n2, n1*n2);
+            printf("%.1lf * %.3lf = %.2lf",n1, n2, n1*n2);//to be executed if operator = *;
             break;
         case '/':
-            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
+            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);//to be executed if operator = /;
             break;
-        default:
+        default:  //code to be executed if operator doesn't match any cases
             printf("Error! operator is not correct");
     }
     return 0;
