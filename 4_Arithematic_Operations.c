fd66b55b097bc994f5cb5c3e10c2d3d54f7fc647
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-18 18:01:37 +0530

    Program to calculate area or perimeter

--- 4_Arithematic_Operations.c
+++ 4_Arithematic_Operations.c
@@ -1,8 +1,10 @@
 //Author AryanSharma 133
+//Creation Date:18/3/2021
+//Last Modification:18/3/2021
 #include <stdio.h> //stdio.h is the header file for standard input and output.
-int main(){        //It means that our will return some integer value at the end.
-    char operator; //Declaring operator variable
-    double n1, n2; //Declaring operator variable
+int main(){       
+    char operator; //Declarartion of variables data type 
+    double n1, n2; //Declarartion of variables data type 
     printf("Enter an operator (+, -, *, /): ");	 //printf function calling
     scanf("%c", &operator);	 //scanf function calling
     printf("Enter two operands: ");	//printf function calling
---
+++ 5_AreaAndPerimeter.c
@@ -0,0 +1,53 @@
+/*
+Author AryanSharma 133
+A program to calculate Area/Perimeter of different shapes.
+Formulas:
+	Perimeter of Square: 4 * side
+	Perimeter of Rectagnle: 2 * (Length + Breadth)
+	Perimeter of Circle: 2*(22/7)*radius
+	Area of Square: side*side
+	Area of Rectangle: Length * Breadth
+	Area of Circle: (22/7)*radius*radius 
+*/ 
+#include <stdio.h>
+int main(){
+	char operation;  //Declarartion of variable data type
+	float radius, sideOfSquare, sideOfRectangle1, sideOfRectangle2;  //Declarartion of variables data type 
+	printf("What do you want to calculate:\n 1.Perimeter of circle.\n 2.Area of circle.\n 3.Perimeter of square.\n 4.Area of square.\n 5.Perimeter of rectangle.\n 5.Area of rectangle.\n");
+	scanf("%c",&operation);
+	switch(operation){ //Switch case function
+		case'1':
+			printf("Enter radius of circle\n");  //Printf function calling.
+			scanf("%f",&radius);  //It will scan the values entered by user. //Taking user input in float data type
+			printf("Perimeter of circle is %f ",((2*22*radius)/7));
+			break;
+		case'2':
+			printf("Enter radius of circle\n");
+			scanf("%f",&radius);	//It will scan the values entered by user.
+			printf("Area of circle is %f", ((22*radius*radius)/7));
+			break;
+		case'3':
+			printf("Enter size of side\n");
+			scanf("%f",&sideOfSquare);	//It will scan the values entered by user.
+			printf("Perimeter of square is %f ",4*sideOfSquare);
+			break;
+		case'4':
+			printf("Enter size of side\n");
+			scanf("%f",&sideOfSquare);	//It will scan the values entered by user.
+			printf("Area of square is %f",sideOfSquare*sideOfSquare);
+			break;
+		case'5':
+			printf("Enter size of sides\n");
+			scanf("%f %f",&sideOfRectangle1, &sideOfRectangle2);  //It will scan the values entered by user.
+			printf("Perimeter of rectangle = %f", 2*(sideOfRectangle1+sideOfRectangle2));
+			break;
+		case'6':
+			printf("Enter size of sides\n");
+			scanf("%f %f",&sideOfRectangle1, &sideOfRectangle2);  //It will scan the values entered by user.
+			printf("Area of rectangle = %f", sideOfRectangle1*sideOfRectangle2);
+			break;
+		default: //code to be executed if operator doesn't match any cases.
+			printf("Error eccoured, input values are incorrect");
+	}
+	return 0;
+}
