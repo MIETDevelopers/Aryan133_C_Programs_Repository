b6ef8dde1d0dc8352a6ec50f8b9e13122ac9d293
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-20 09:02:53 +0530

    Hello World

--- 3_Hello.c
+++ 3_Hello.c
@@ -1,5 +1,7 @@
-//Author Aryan Sharma 133
-#include <stdio.h>
+//Author AryanSharma 133
+//Creation Date:18/3/2021
+//Last Modification:18/3/2021
+#include <stdio.h>  //stdio.h is the header file for standard input and output.
 int main(){
 	printf("Hello World"); //It will print "Hello World"
 	return 0;
