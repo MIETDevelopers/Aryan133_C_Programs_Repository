e3d35bb10536585659b4108d541abca4921bb781
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-20 13:37:57 +0530

    Program to find maximum number amoung 'n' numbers

--- 12_MaximumAmoungUs.c
+++ 12_MaximumAmoungUs.c
@@ -4,22 +4,19 @@ Creation Date:20/3/2021
 Last Modification:20/3/2021
 Purpose: To find maximum number in n number.
 */
-#include <stdio.h>
+#include <stdio.h>  //stdio.h is the header file for standard input and output.
 int main(){
-  int i,n;
-  float num,max=0;
-  //It will set the maximum number (Limit) of values to be entered by user based on their choice.
-  printf("How many numbers: ");
-  scanf("%d",&n); 
-  
-  for(i=0; i<n; i++)
-  {
-    //It will scan the numbers entered by user upto the limit setted by user.
-    printf("\nEnter number %d: ",i+1);
-    scanf("%f",&num);
-    if(num>max)
-    max=num;
-  }
-  printf("\nThe Largest number is %f",max); //It will give the largest number as the output.
-  return 0;
+  	int i,n;  //Variable declarartion of integer data type.
+  	float num; //Variable declarartion of float data type.
+  	float max=0;  //Variable declarartion and initilisation of float data type.
+  	printf("How many numbers: "); //printf function calling.
+  	scanf("%d",&n); 
+	for(i=0; i<n; i++){  
+		printf("\nEnter number %d: ",i+1);
+    	scanf("%f",&num);  //It will scan the numbers entered by user upto the limit setted by user because of for loop.
+    	if(num>max)
+    		max=num;  //Putting value of num in max.
+ 	}
+	printf("\nThe Largest number is %f",max); //printf function calling  //It will print the largest number as the output.
+ 	return 0;
 }
