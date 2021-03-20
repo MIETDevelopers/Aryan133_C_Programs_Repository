c84aa198be9d868f22bc55c0212e1c869152604d
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-20 09:17:25 +0530

    Program to check entered alphabet is vowel or consonant

--- 9_VowelOrConsonants.c
+++ 9_VowelOrConsonants.c
@@ -7,7 +7,7 @@ The letters a, e, i, o and u are called vowels & the remaining ones are called c
 */
 #include<stdio.h>
 int main(){
-    char c, alphabet;  //Declarartion of variable data type.
+    char c, alphabet;  //Variable declarartion of character data type.
     printf("Enter an alphabet:\n");  //Printf function calling.
     scanf("%c", &c); //It will scan the alphabet entered by user.
     alphabet = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
