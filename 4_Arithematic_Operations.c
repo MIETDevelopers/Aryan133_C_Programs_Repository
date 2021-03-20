3f4ecf156420aa91045a851ce84f6941677a442e
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-20 09:05:37 +0530

    Aprogram to  calculate simple Arithematic Calculator

--- 4_Arithematic_Operations.c
+++ 4_Arithematic_Operations.c
@@ -1,10 +1,11 @@
 //Author AryanSharma 133
 //Creation Date:18/3/2021
 //Last Modification:18/3/2021
+//Program of simple calculator.
 #include<stdio.h> //stdio.h is the header file for standard input and output.
 int main(){       
-    char operator; //Declarartion of variables data type 
-    double n1, n2; //Declarartion of variables data type 
+    char operator; //Variable declarartion of character data type 
+    double n1, n2; //Variable declarartion of double data type 
     printf("Enter an operator (+, -, *, /): ");	 //printf function calling
     scanf("%c", &operator);	 //scanf function calling
     printf("Enter two operands: ");	//printf function calling
