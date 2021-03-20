7969d9f2823ade44beb59d83f058cb08ff382fc1
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-20 09:19:14 +0530

    A C program to find factorial

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -6,12 +6,12 @@ Purpose: Program to calculate Factorial of a number.
 */
 #include<stdio.h>  
 int main(){    
-	int i,fact=1,number;    
+	int i,fact=1,number;   //Variable declarartion of integer data type.
 	printf("Enter a number: ");  //Printf function calling.   
 	scanf("%d",&number);    //It will scan the number entered by user.
 	for(i=1;i<=number;i++){    //For loop
 		fact=fact*i;   //Formula to calculate Factorial.
 		}    
-	printf("Factorial of %d is: %d",number,fact);
+	printf("Factorial of %d is: %d",number,fact);  //printf function calling.
 	return 0;  
 }
