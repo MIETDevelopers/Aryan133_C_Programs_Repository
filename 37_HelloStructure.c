7e498aa1df065ce974cd479da1c9e64a1b7a8213
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-30 22:42:21 +0530

    Hello World that would return to a structure data type.

---
+++ 37_HelloStructure.c
@@ -0,0 +1,15 @@
+/*
+Author Aryan Sharma 133
+Creation Date:30/3/2021
+Last Modification:30/3/2021
+Purpose: Program of Hello World that would return to a structure data type.
+*/
+#include<stdio.h> 		//Including standard input ouput header files from library.
+struct s {
+	char text[50];		//Variable declarartion of character data type.
+	};
+int main(){			//main function body.
+	struct s a[10];	
+	printf("Hello Buddy!!");    //Printf function calling. 
+	return 's';
+}
