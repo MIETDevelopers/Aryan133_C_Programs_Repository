796c4f1aca0941a14b5c0e3c8684887ad4e30797
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-30 22:10:25 +0530

    Sum of array

---
+++ 32_SumArr.c
@@ -0,0 +1,23 @@
+/*
+Author Aryan Sharma 133
+Creation Date:30/3/2021
+Last Modification:30/3/2021
+Purpose: Program to find sum of all elements of array.
+*/
+#include <stdio.h>      //stdio.h is the header file for standard input and output.
+#define MAX_SIZE 100
+int main(){
+    int arr[MAX_SIZE];      //Variable declarartion of integer data type.
+    int i, n, sum=0;        //Variable declarartion of integer data type.
+    printf("Enter size of the array: ");    //Printf function calling.
+    scanf("%d", &n);    //scanf function calling.
+    printf("Enter %d elements in the array: ", n);  //Printf function calling.
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);   //scanf function calling.
+    }
+    for(i=0; i<n; i++){
+        sum = sum + arr[i]; //Add each array element to sum
+    }
+    printf("Sum of all elements of array = %d", sum);  //Printf function calling.
+    return 0;
+}
