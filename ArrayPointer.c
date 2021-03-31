743e8d29c1b88a5f6af750b951a308dbf025addb
Arayan Sharma <2020a1r133@mietjammu.in>
2021-03-31 15:10:49 +0530

    Array name as a pointer reprsentation and print

---
+++ ArrayPointer.c
@@ -0,0 +1,24 @@
+/*
+Author AryanSharma 133
+Creation Date:18/3/2021
+Last Modification:18/3/2021
+Array name as pointer representation and print its elements values
+*/
+#include <stdio.h> //Pre process directive to include standard input output functions header file
+int main () { //main function body starting
+   /* an array with 5 elements */
+   double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+   double *p;
+   int i;
+   p = balance;
+   /* output each array element's value */
+   printf( "Array values using pointer\n");	
+   for ( i = 0; i < 5; i++ ) {
+      printf("*(p + %d) : %f\n",  i, *(p + i) );
+   }
+   printf( "Array values using balance as address\n");	//printf function calling
+   for ( i = 0; i < 5; i++ ) {
+      printf("*(balance + %d) : %f\n",  i, *(balance + i) );
+   }
+   return 0;   //return statemnet 
+}
